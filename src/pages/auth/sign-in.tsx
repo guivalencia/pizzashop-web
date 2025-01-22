a5270f1e8878b4eeb518e189f74722e09c0bf508
Guilherme Valencia <guilhermevalencia@gmail.com>
2025-01-22 13:30:47 -0400

    configuração inicial finalizada

---
+++ src/pages/auth/sign-in.tsx
@@ -0,0 +1,10 @@
+import { Helmet } from "react-helmet-async"
+
+export function SigIn() {
+    return (
+        <>
+            <Helmet title="Login" />
+            <h1>SigIn</h1>
+        </>
+    )
+}
