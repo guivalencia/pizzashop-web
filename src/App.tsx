dbdf4975d0587ff2ef27be5efeb943e6ec363819
Guilherme Valencia <guilhermevalencia@gmail.com>
2025-01-28 14:52:35 -0400

    configurando conexao com api backend

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import './global.css'
 import { router } from './routes'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { ThemeProvider } from './components/theme/theme-provider'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
 
 export function App() {
   return (
@@ -11,7 +13,9 @@ export function App() {
       <ThemeProvider storageKey='pizzasho-theme' defaultTheme='dark'>
         <Helmet titleTemplate='%s | pizza.shop' />
         <Toaster richColors />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   )
