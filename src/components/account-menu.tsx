4b1438f31a8676fbf1524582da54992da8b7c2d7
guivalencia <guilhermevalencia@gmail.com>
2025-02-03 19:28:00 -0400

    alteração de dados e signout

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -1,31 +1,76 @@
 import { Building, ChevronDown, LogOut } from "lucide-react";
 import { Button } from "./ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { getProfile } from "@/api/get-profile";
+import { getManagedRestaurant } from "@/api/get-managed-restaurant";
+import { Skeleton } from "./ui/skeleton";
+import { Dialog, DialogTrigger } from "./ui/dialog";
+import { StoreProfileDialog } from "./store-profile-dialog";
+import { useNavigate } from "react-router-dom";
+import { signOut } from "@/api/sign-out";
 
-export function AccountMenu(){
+export function AccountMenu() {
+    const navigate = useNavigate()
+
+    const { data: profile, isLoading: isLoadingProfile } = useQuery({
+        queryKey: ['profile'],
+        queryFn: getProfile
+    })
+
+    const { data: managedRestaurant, isLoading: isLoadingManagedRestaurant } = useQuery({
+        queryKey: ['managed-restaurant'],
+        queryFn: getManagedRestaurant,
+        staleTime: Infinity
+    })
+
+    const { mutateAsync: signOutFn, isPending: isSigningOut } = useMutation({
+        mutationFn: signOut,
+        onSuccess: () => {
+            navigate('/sign-in', { replace: true })
+        }
+    })
+
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger>
-                <Button variant="outline" className="flex items-center gap-2 select-none">
-                    Pizza Shop
-                    <ChevronDown className="w-4 h-4" />
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end" className="w-56">
-                <DropdownMenuLabel className="flex flex-col">
-                    <span>Guilherme Valencia</span>
-                    <span className="text-xs font-normal text-muted-foreground">guilherme@gmail.com.br</span>
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                    <Building className="mr-2 h-4 w-4" />
-                    <span>Perfil da loja</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
-                    <LogOut className="mr-2 h-4 w-4" />
-                    <span>Sair</span>
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
+        <Dialog>
+
+            <DropdownMenu>
+                <DropdownMenuTrigger>
+                    <Button variant="outline" className="flex items-center gap-2 select-none">
+                        {isLoadingManagedRestaurant ? (<Skeleton className="h-4 w-40" />) : (managedRestaurant?.name)}
+                        <ChevronDown className="w-4 h-4" />
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end" className="w-56">
+                    <DropdownMenuLabel className="flex flex-col">
+                        {isLoadingProfile ? (
+                            <div className="space-y-1.5">
+                                <Skeleton className="h-4 w-32" />
+                                <Skeleton className="h-3 w-24" />
+                            </div>
+                        ) : (
+                            <>
+                                <span>{profile?.name}</span>
+                                <span className="text-xs font-normal text-muted-foreground">{profile?.email}</span>
+                            </>
+                        )}
+                    </DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DialogTrigger asChild>
+                        <DropdownMenuItem>
+                            <Building className="mr-2 h-4 w-4" />
+                            <span>Perfil da loja</span>
+                        </DropdownMenuItem>
+                    </DialogTrigger>
+                    <DropdownMenuItem asChild className="text-rose-500 dark:text-rose-400" disabled={isSigningOut}>
+                        <button className="w-full" onClick={() => signOutFn()}>
+                            <LogOut className="mr-2 h-4 w-4" />
+                            <span>Sair</span>
+                        </button>
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+            <StoreProfileDialog />
+        </Dialog>
     )
 }
